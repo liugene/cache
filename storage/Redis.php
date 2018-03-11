dc989cda1735900b29f8443c30434c27f3115669
liugene <750688237@qq.com>
2018-03-11 19:02:16 +0800

    init

---
+++ storage/Redis.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace links\cache\storage;
+
+use links\cache\Storage;
+
+class Redis extends Storage
+{
+
+    public function put($key, $value)
+    {
+        // TODO: Implement put() method.
+    }
+
+    public function get($key)
+    {
+        // TODO: Implement get() method.
+    }
+
+}
