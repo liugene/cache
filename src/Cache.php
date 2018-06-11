f7d1e667d1eb91c351b25da5c575f6ae9181b8c9
liugene <750688237@qq.com>
2018-06-11 21:25:16 +0800

    完善cache

--- src/Cache.php
+++ src/Cache.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * --------------------------------------------------*
- *  LhinkPHP遵循Apache2开源协议发布  Link ALL Thing  *
- * --------------------------------------------------*
- *  @author LiuJun     Mail-To:liujun2199@vip.qq.com *
- * --------------------------------------------------*
- * Copyright (c) 2017 LinkPHP. All rights reserved.  *
- * --------------------------------------------------*
- *                 LinkPHP缓存入口文件               *
- * --------------------------------------------------*
- */
-
 namespace linkphp\cache;
 
 use linkphp\cache\storage\File;
@@ -54,8 +42,6 @@ class Cache
     {
         return $this->storage()->put($key,$data);
     }
-
-    public function remove(){}
 
     public function delete(){}
 
